293d9e0c71c0054761e14cadb2c57f7a2a29b035
BlenderRender <Blenderrender@gmail.com>
2012-05-02 21:56:27 -0300

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -1,32 +1,47 @@
+#Imports
 import mutagen
 import glob
 import os
 import re
 
+#Metadata Variables
 artist = ''
 album = ''
 title = ''
+
+#Array for re parseing
 split = []
+#Counts total songs
 count = 0
+
+#Path variable for mp3 folder (change accordingly)
 path = 'mp3/'
 
 from mutagen.mp4 import MP4
 from mutagen.easyid3 import EasyID3
 
+#Runs through the file structure - Artist / Album / SongTitle .m4a
 for infile in glob.glob( os.path.join(path, '*/*/*.m4a') ):
 
-		print count
+		#Parses mp3 file structure using '.' '/' and '-' delimiters	
 		split = re.split('\.|/|-', infile)
+		#Sets variables and replaces underscores with white space
 		artist = re.sub('_', " ", split[1])
 		album = re.sub('_', " ", split[2])
 		title = re.sub('_', " ", split[4])
+		
+		#Optional Print Statements & Counter
+		print count
 		print "artist: " + artist
 		print "album: " + album
 		print "title: " + title
 		count = count + 1
-
+		
+		#Sets Metadata tags
 		audio = mutagen.File(infile, easy=True)
 		audio["title"] = title
 		audio["album"] = album
 		audio["artist"] = artist
 		audio.save()
+		
+		
